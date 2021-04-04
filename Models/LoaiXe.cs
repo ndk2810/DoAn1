7346a967311d516c5c7ef8af693b6747062f5f7f
ndk2810 <khangdeptrai2810@gmail.com>
2021-04-04 19:33:27 +0700

    New HeThongThueXe

---
+++ Models/LoaiXe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeThongThueXe.Models
+{
+    public class LoaiXe
+    {
+        public string tenLoaiXe { get; set; }
+        public string linkImgLoaiXe { get; set; }
+    }
+}

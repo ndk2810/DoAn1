6c858c10f904424207c89abee14d54511f5141ca
Thang <Thang@DESKTOP-H3THSAV>
2021-04-11 13:54:31 +0700

    Create TinTuc page

---
+++ Models/Xe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeThongThueXe.Models
+{
+    public class Xe
+    {
+        public string tenLoaiXe { get; set; }
+        public string linkImgLoaiXe { get; set; }
+    }
+}

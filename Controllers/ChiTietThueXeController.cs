30ce11153bf43186f997119bce3ef106c8561e02
ndk2810 <khangdeptrai2810@gmail.com>
2021-04-22 10:17:29 +0700

    Database fix

---
+++ Controllers/ChiTietThueXeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeThongThueXe.Models;
+
+namespace HeThongThueXe.Controllers
+{
+    public class ChiTietThueXeController : Controller
+    {
+        private THUEXEEntities db = new THUEXEEntities();
+        // GET: ThueXe4Cho
+        public ActionResult Index(int idLoaiXe)
+        {
+            LOAIXE LoaiXe = db.LOAIXEs.Find(idLoaiXe);
+            ViewBag.LoaiXe = LoaiXe;
+            return View();
+        }
+    }
+}

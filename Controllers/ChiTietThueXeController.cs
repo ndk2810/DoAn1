201fedba491d1bec8415db6934754afc742697b3
ndk2810 <khangdeptrai2810@gmail.com>
2021-04-29 19:57:59 +0700

    Gui yeu cau dat xe 1.0

--- Controllers/ChiTietThueXeController.cs
+++ Controllers/ChiTietThueXeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HeThongThueXe.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace HeThongThueXe.Controllers
 {
@@ -15,6 +16,31 @@ namespace HeThongThueXe.Controllers
         {
             LOAIXE LoaiXe = db.LOAIXEs.Find(idLoaiXe);
             ViewBag.LoaiXe = LoaiXe;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemYeuCau(YeuCauThueCLASS yeuCauThue)
+        {
+            YEUCAUTHUE newYeuCauThue = new YEUCAUTHUE();
+            KHACH newKhach = new KHACH();
+            newKhach.TenKhach = yeuCauThue.TenKhach;
+            newKhach.DiaChi = yeuCauThue.DiaChi;
+            newKhach.Email = yeuCauThue.Email;
+            newKhach.SDT = yeuCauThue.DienThoai;
+
+            newYeuCauThue.IDHieuXe = yeuCauThue.IDHieuXe;
+            newYeuCauThue.IDLoaiXe = yeuCauThue.IDLoaiXe;
+            newYeuCauThue.NgayTao = DateTime.Now;
+            newYeuCauThue.ThoiGianThue = DateTime.Now;
+            newYeuCauThue.ThoiGianTra = DateTime.Now;
+            newYeuCauThue.YeuCauKhac = yeuCauThue.YeuCauKhac;
+
+            //db.YEUCAUTHUEs.Add(newYeuCauThue);
+            //db.SaveChanges();
+
+            ViewBag.YCT = yeuCauThue;
+
             return View();
         }
     }
--- Controllers/LoaiXeChoThueController.cs
+++ Controllers/LoaiXeChoThueController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HeThongThueXe.Models;
 
 namespace HeThongThueXe.Controllers
 {
     public class LoaiXeChoThueController : Controller
     {
-        // GET: LoaiXeChoThue
-        public ActionResult Index()
+        private THUEXEEntities db = new THUEXEEntities();
+        // GET: ThueXe4Cho
+        public ActionResult Index(int idHieuXe)
         {
+            HIEUXE HieuXe = db.HIEUXEs.Find(idHieuXe);
+            ViewBag.HieuXe = HieuXe;
             return View();
         }
     }

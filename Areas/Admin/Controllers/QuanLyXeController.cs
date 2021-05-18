d7c7843a1aaa07ccd3798e4f032c216129808367
MinhPhat <phannguyenminhphat@gmail.com>
2021-05-18 21:17:35 +0700

    Chinh sua frontend

---
+++ Areas/Admin/Controllers/QuanLyXeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeThongThueXe.Areas.Admin.Controllers
+{
+    public class QuanLyXeController : Controller
+    {
+        // GET: Admin/QuanLyXe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChiTietXe()
+        {
+            return View();
+        }
+    }
+}

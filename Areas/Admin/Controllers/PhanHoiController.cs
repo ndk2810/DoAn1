768a462bca1485f1a31cdccb10467088b478bdb3
ndk2810 <khangdeptrai2810@gmail.com>
2021-05-21 23:12:15 +0700

    gop phan hoi

--- Areas/Admin/Controllers/PhanHoiController.cs
+++ Areas/Admin/Controllers/PhanHoiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
 using HeThongThueXe.Models;
 
 namespace HeThongThueXe.Areas.Admin.Controllers
@@ -21,6 +23,31 @@ namespace HeThongThueXe.Areas.Admin.Controllers
         {
             PHANHOI phanHoi = db.PHANHOIs.Find(idPhanHoi);
             ViewBag.phanHoi = phanHoi;
+            return View();
+        }
+        public ActionResult TraLoiPhanHoi(int idPhanHoi)
+        {
+            PHANHOI phanHoi = db.PHANHOIs.Find(idPhanHoi);
+            ViewBag.phanHoi = phanHoi;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TraLoiPhanHoi(HeThongThueXe.Models.gmail model)
+        {
+            MailMessage mm = new MailMessage("huy98796543@gmail.com", model.To);
+            mm.Subject = model.Subject;
+            mm.Body = model.Body;
+            mm.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            NetworkCredential nc = new NetworkCredential("tai98796543@gmail.com	", "nguyentai4$");
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+            ViewBag.Message = "Gửi mail thành công";
             return View();
         }
     }
